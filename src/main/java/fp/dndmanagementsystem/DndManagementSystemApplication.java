5f17f560ee636571dc24aea79cfe1b534ac55381
User <preslava.sv@gmail.com>
2024-07-17 22:01:17 +0300

    foundations-v00

---
+++ src/main/java/fp/dndmanagementsystem/DndManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package fp.dndmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DndManagementSystemApplication.class, args);
+    }
+
+}
