158a6d52d7b5f2ea50dc37347f4a58171953a51d
User <preslava.sv@gmail.com>
2024-07-24 23:02:08 +0300

    registration + login controllers updated

--- src/main/java/fp/dndmanagementsystem/web/LoginController.java
+++ src/main/java/fp/dndmanagementsystem/web/LoginController.java
@@ -2,14 +2,12 @@ package fp.dndmanagementsystem.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("/users")
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
-        return "auth-login";
+    public String index() {
+        return "login";
     }
 }
--- src/main/java/fp/dndmanagementsystem/web/RegistrationController.java
+++ src/main/java/fp/dndmanagementsystem/web/RegistrationController.java
@@ -2,14 +2,13 @@ package fp.dndmanagementsystem.web;
 
 import fp.dndmanagementsystem.model.dto.UserRegistrationDTO;
 import fp.dndmanagementsystem.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/users")
 public class RegistrationController {
 
     private final UserService userService;
@@ -18,24 +17,14 @@ public class RegistrationController {
         this.userService = userService;
     }
 
-    @ModelAttribute("registerDTO")
-    public UserRegistrationDTO registerDTO() {
-        return new UserRegistrationDTO();
-    }
-
     @GetMapping("/register")
-    public String register() {
-        return "auth-register";
+    public String register(@ModelAttribute("registerData") UserRegistrationDTO registerData) {
+        return "register";
     }
 
-//    @WarnIfExecutionExceeds(
-//            threshold = 1000
-//    )
     @PostMapping("/register")
-    public String register(UserRegistrationDTO registerDTO) {
-
-        userService.registerUser(registerDTO);
-
-        return "redirect:/";
+    public String doRegister(@Valid UserRegistrationDTO registerData) {
+        this.userService.registerUser(registerData);
+        return "redirect:/login";
     }
 }
