7f3bfeaf68e924bfff11eaf07cdbfabfcbdccdcc
User <preslava.sv@gmail.com>
2024-07-24 22:56:30 +0300

    init.java added - needs completion

---
+++ src/main/java/fp/dndmanagementsystem/init/init.java
@@ -0,0 +1,12 @@
+package fp.dndmanagementsystem.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class init implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
