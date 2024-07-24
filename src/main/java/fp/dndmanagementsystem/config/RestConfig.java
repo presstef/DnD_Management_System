2dc2aaf2a9d0b71b09ae16c733d5bdb2a9dae33b
User <preslava.sv@gmail.com>
2024-07-24 22:47:58 +0300

    rest configuration file added

--- src/main/java/fp/dndmanagementsystem/config/RestConfig.java
+++ src/main/java/fp/dndmanagementsystem/config/RestConfig.java
@@ -1,51 +1,12 @@
 package fp.dndmanagementsystem.config;
-
-import fp.dndmanagementsystem.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestConfig {
-    @Bean("genericRestClient")
-    public RestClient genericRestClient() {
-        return RestClient.create();
-    }
-
-    @Bean("campaignsRestClient")
-    public RestClient campaignsRestClient(CampaignApiConfig campaignApiConfig,
-                                       ClientHttpRequestInterceptor requestInterceptor) {
-        return RestClient
-                .builder()
-                .baseUrl(campaignApiConfig.getBaseUrl())
-                .defaultHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                .requestInterceptor(requestInterceptor)
-                .build();
-    }
-
     @Bean
-    public ClientHttpRequestInterceptor requestInterceptor(UserService userService,
-                                                           JwtService jwtService) {
-        return (r, b, e) -> {
-            userService
-                    .getCurrentUser()
-                    .ifPresent(mud -> {
-                        String bearerToken = jwtService.generateToken(
-                                mud.getUuid().toString(),//
-                                Map.of(
-                                        "roles",
-                                        mud.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList()
-                                )
-                        );
-
-                        System.out.println("BEARER TOKEN: " + bearerToken);
-
-                        r.getHeaders().setBearerAuth(bearerToken);
-                    });
-
-            return e.execute(r, b);
-        };
+    public RestClient restClient() {
+        return RestClient.create();
     }
 }
